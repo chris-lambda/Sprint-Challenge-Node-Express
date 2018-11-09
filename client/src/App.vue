4e63ce5f30704ddec80d28062afdf6ef447c87ad
Stublink <chrisrey95@gmail.com>
2018-11-09 16:44:47 -0500

    :sparkles: Vue front end

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <UserList />
+  </div>
+</template>
+
+<script>
+import UserList from './components/UserList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    UserList
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img[alt="Vue logo"] {
+  width: 100px;
+  height: auto;
+}
+
+</style>
---
+++ client/src/components/User.vue
@@ -0,0 +1,100 @@
+<template lang="pug">
+    div.container
+        div.user-container(v-bind:class='{placeholder: editMode}')
+            h1.user-name {{project.name}}
+            div.post-container(v-if='tasks.length > 0')
+                Post(v-bind:text='tasks[0].description' v-bind:postId='tasks[0].id')
+            button.button.edit-button(
+                type="button" 
+                @click="editModeToggle(project, tasks)"
+            ) EDIT
+</template>
+
+<script>
+    import axios from 'axios';
+
+    import Post from './Post.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        name: 'User',
+        components: {
+            Post,
+            UserEdit
+        },
+        data: () => {
+            return {
+                tasks: [],
+                editMode: false
+            }
+        },
+        props: {
+            project: Object,
+            editModeToggle: Function,
+        },
+        methods: {},
+        created () {
+            axios.get(`http://localhost:9000/api/projects/actions/${this.project.id}`)
+                .then(res => this.tasks = res.data)
+                .catch(error => console.error(error));
+        }
+    }
+
+</script>
+
+<style lang="sass">
+
+    .container 
+        width: 100%
+        height: 100%
+        position: relative
+    
+    .user-container
+        width: 300px
+        min-height: 250px
+        border: 1px solid rgba(0,0,0,0.1)
+        border-radius: 8px
+        margin: 10px
+        display: flex
+        flex-direction: column
+        justify-content: flex-start
+        padding-bottom: 20px
+        background: white
+        padding: 0px 15px
+        
+        &:hover 
+            transition: all .212s ease-in
+            box-shadow: 1px 1px 8px -1px rgba(0,0,0,0.4)
+        .user-name
+            font-size: 22px
+            font-weight: 700
+        h1
+            user-select: none
+
+    .placeholder
+        filter: blur(3px)
+
+    .button 
+        width: 200px
+        height: 35px
+        position: absolute
+        bottom: 20px
+        left: 50%
+        margin-left: -100px
+        border: none
+        border-radius: 12px
+        border: 1px solid rgba(0,0,0,0.1)
+        cursor: pointer
+        font-weight: 700
+        outline: none
+        &:hover
+            transition: all .212s ease-in
+            box-shadow: 0px 0px 3px -1px rgba(0,0,0,0.4)
+
+    .edit-button
+        &:hover
+            color: #41B883
+    .delete-button
+        &:hover
+            color: rgb(184,65,131)
+</style>
